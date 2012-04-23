8224cbd7ef91543e7102cd49a65a93f0ad6eba74
Evan Sheehan <wallace.sheehan@gmail.com>
2012-04-22 20:40:01 -0400

    Got branches coming off head and hands now. No dynamic growth.

---
+++ src/ent.cpp
@@ -0,0 +1,9 @@
+//
+//  ent.cpp
+//  Be A Tree
+//
+//  Created by Evan Sheehan on 4/21/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#include <iostream>
---
+++ src/ent.h
@@ -0,0 +1,14 @@
+//
+//  ent.h
+//  Be A Tree
+//
+//  Created by Evan Sheehan on 4/21/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#ifndef Be_A_Tree_ent_h
+#define Be_A_Tree_ent_h
+
+
+
+#endif
